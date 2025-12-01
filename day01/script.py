c185575dd8926180cee5e3f88e54d576ef9d773f
Dan Peake <148717808+DanPeake@users.noreply.github.com>
2025-12-02 00:27:46 +0100

    Cleaned up day 1, added day 2

--- day01/script.py
+++ day01/script.py
@@ -1,32 +1,37 @@
 with open('data.txt') as f: raw = f.read()
+
+# We have a dial that is numbered 0-99, and a sequence of left/right turns to perform
+# - The dial starts at position 50
+# - The password is the number of times the dial stops on 0 after each step in the sequence
+
 #raw = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
 data = raw.strip().split()
-turns = [int(('-' if s[:1] == 'L' else '') + s[1:]) for (s) in data]
+
+# Convert the L/R turn data into +/- ints for easier use
+sequence = [int(('-' if s[:1] == 'L' else '') + s[1:]) for (s) in data]
 
 dial = 50
 count = 0
-for turn in turns:
-    dial += turn
-    while dial > 99:
-        dial -= 100
-    while dial < 0:
-        dial += 100
+for rotation in sequence:
+    # Do the turns and shift the result back into the 0-99 range using modulo
+    dial = (dial + rotation) % 100
     if dial == 0:
         count += 1
 
-print('1.',count)
+print('1. Password:', count)
+
+########################################
+
+# - The password is the number of times the dial passes or stops on 0 during each step
 
 dial = 50
 count = 0
-for turn in turns:
-    step = 1 if turn > 0 else -1
-    for _ in range(abs(turn)):
-        dial += step
-        if dial < 0:
-            dial += 100
-        elif dial > 99:
-            dial -= 100
+# We are going to do each individual turn in each rotation of the sequence so we can count the 0s
+for rotation in sequence:
+    turn = 1 if rotation > 0 else -1
+    for _ in range(abs(rotation)):
+        dial = (dial + turn) % 100
         if dial == 0:
             count += 1
 
-print('2.',count)
+print('2. Password:', count)
--- day02/script.py
+++ day02/script.py
@@ -1,4 +1,4 @@
 with open('data.txt') as f: raw = f.read()
-#raw = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
+#raw = ""
 data = raw.strip().split()
 
